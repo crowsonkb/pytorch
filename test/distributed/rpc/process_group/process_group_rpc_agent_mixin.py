b684d8ae5b16739b570001e1adc093c5ea6aba51
Shihao Xu <xsh6528@gmail.com>
2019-11-13 12:54:25 -0800

    Add `RpcAgentOptions` struct type, which bundles different required arguments for different `RpcAgent`s
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/29443
    
    https://github.com/pytorch/pytorch/pull/28226 introduced `worker_to_id` arg to the `def init_model_parallel` function for other `RpcAgent`. While it's not really used by `ProcessGroupAgent`. Cleanup is wanted for this, as described in #29031.
    
    To adapt to the difference of different `RpcAgent`, adding a `RpcAgentOptions` base classes, which allow leveraging inheritance to add extra fields.
    
    closes #29031
    ghstack-source-id: 93845746
    
    Differential Revision: [D18283849](https://our.internmc.facebook.com/intern/diff/D18283849/)

--- test/distributed/rpc/process_group/process_group_rpc_agent_mixin.py
+++ test/distributed/rpc/process_group/process_group_rpc_agent_mixin.py
@@ -5,3 +5,11 @@ class ProcessGroupRpcAgentMixin(object):
     @property
     def rpc_backend_name(self):
         return "PROCESS_GROUP"
+
+    @property
+    def rpc_agent_options(self):
+        return rpc.backend_registry.construct_rpc_agent_options(
+            self.rpc_backend,
+            # Use enough 'num_send_recv_threads' until we fix https://github.com/pytorch/pytorch/issues/26359
+            num_send_recv_threads=16,
+        )
